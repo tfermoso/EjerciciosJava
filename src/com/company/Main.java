27ece5693c181aa13a5d3319e35d222a8ba6da4c
Santi <sjamaldeite@gmail.com>
2020-07-21 19:54:15 +0200

    Loteria(Ejercicio 7)[arreglado]

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,40 +1,39 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
-        public class Main {
+public class Main {
 
-            public static void main(String[] args) {
-                //    Tenemos un array de enteros de tamaño 20. Realizar un algoritmo que vaya leyendo números y los guarde
-                //    en el array, pero si la suma de los números leídos alcanza o supera 100, se dejarán de leer números
-                //    y rellenará con 0 las posiciones que queden libres.
-                Scanner leer= new Scanner(System.in);
-                int[] numeros = new int [20];
-                boolean acabado=false;
-                int contador=0;
-                int numero;
-                do {
-                    System.out.println("Introduzca numero");
-                    numero = leer.nextInt();
-                    leer.nextLine();
-                    numeros[contador]=numero;
+    public static void main(String[] args) {
+        // Realizar un algortimo que genere una combinación de la lotería primitiva. El objetivo es
+        // generar 6 números aleatorios entre 1 y 49 que no se repitan. Puedes utilizar la
+        // función GenerarAleatorio utilizada en los ejemplos del manual
+        int contador=0;
+        int[] loteria=new int[6];
+        Random random = new Random();
+        do {
+            int upperbound = 49;
+            int numero = random.nextInt(upperbound);
+            if (numero != 0) {
+                if (comprobarArray(numero, loteria, contador) == true) {
                     contador++;
-                    int suma=0;
-                    for (int i = 0; i <numeros.length ; i++) {
-                        suma+=numeros[i];
-                        if (suma>=100){
-                            acabado=true;
-                        }else if(contador==19){
-                            acabado=true;
-                        }
-                    }
-                }while (!acabado);
-                for (int i = 0; i <numeros.length ; i++) {
-                    if (numeros[i]!=0){
-                        System.out.println(numeros[i]);
-                    }
                 }
             }
+        }while (contador<6);
+        for (int i = 0; i <loteria.length ; i++) {
+            System.out.println(loteria[i]);
         }
+    }
 
+    private static boolean comprobarArray(int numero, int[] loteria,int contador) {
+        boolean presente=false;
+        for (int i = 0; i <loteria.length ; i++) {
+            if (numero==loteria[i]&&numero!=0){
+                presente=true;
+            }
+        } if (presente!=true){
+            loteria[contador]=numero;
+        }
+        return presente;
+    }
+}

2687d4c42c7e7fd5b5e85452d3ce2908423ad11f
david <fremenida@gmail.com>
2020-07-21 20:15:09 +0200

    Preactica 8 array multidimensional

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,99 +8,35 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-        //Scanner leer = new Scanner(System.in);
-        //int[] numeros = new int[20];
 
-        int[] loteria=new int[6];
-        int contador=0;
-        //Genero numero aleatorio
-        Random rand = new Random();
-        do {
-            //int upperbound = 49;
-            Integer numero = rand.nextInt(50);
-
-            //Compruebo si ya lo tengo
-        if(!comprobarArray(numero,loteria)){
-            loteria[contador]=numero;
-            contador++;
-
-
-            //for (int i = 0; i <loteria.length ; i++) {
-                //if (numero!=loteria[1]&&numero!=loteria[2]&&numero!=loteria[3]&&numero!=loteria[4]
-                       // &&numero!=loteria[5]&&numero!=loteria[0]&& numero!=0){
-                    //loteria[contador]=numero;
-                   // contador++;
-               // }
+        String[][] temperaturas=new String[10][2];
+        Scanner leer=new Scanner(System.in);
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("Nombre ciudad: ");
+            String ciudad=leer.nextLine();
+            System.out.println("Temperatura: ");
+            String temp=leer.nextLine();
+            temperaturas[i][0]=ciudad;
+            temperaturas[i][1]=temp;
+        }
+        double media=0.0;
+        for (int i = 0; i <temperaturas.length ; i++) {
+            media+=convertirEntero(temperaturas[i][1]);
+        }
+        media=media/10;
+        System.out.println("La temperatura media es: "+media);
+        for (int i = 0; i <temperaturas.length ; i++) {
+            if (convertirEntero(temperaturas[i][1])<media){
+                System.out.println("Ciudad: "+temperaturas[i][0]+", temperatura: "+temperaturas[i][1]);
             }
-        }while (contador<6);
-        for (int i = 0; i <loteria.length ; i++) {
-            System.out.println(loteria[i]+",");
+
         }
-        System.out.println("");
+    }
+    public static int convertirEntero(String valor){
+        return Integer.parseInt(valor);
     }
 
-    private static boolean comprobarArray(Integer numero, int[] loteria) {
-        for (int i = 0; i < loteria.length; i++) {
-            if(loteria[i]==numero) return true;
-        }
-        return false;
     }
-}
 
-
-
-
-//        int[] numeros=new int[5];
-//        System.out.println("Introduce 5 números positivos");
-//        int contador=0,num;
-//        do{
-//            System.out.println("Introduce un número");
-//            num=leer.nextInt();
-//            leer.nextLine();
-//            if(num>0){
-//                numeros[contador]=num;
-//                contador++;
-//            }
-//        }while(contador<5);
-//        System.out.println("numeros introducidos: ");
-//        for (int i = 4; i >=0 ; i--) {
-//            System.out.print(numeros[i]+",");
-//        }
-//        System.out.println("");
-//    }
-
-            //if (num<0) {
-                //numeros[contador]=num;
-            //}else {
-                //numeros[contador]=num;
-                //contador++;
-
-        //}while (contador<5);
-        //System.out.println(numeros[4]+"\n"+numeros[3]+"\n"+numeros[2]+"\n"+numeros[1]+"\n"+numeros[0]);
-    //}
-        //System.out.println("Introduce número a buscar");
-        //int numero=leer.nextInt();
-        //leer.nextLine();
-        //int contador=0;
-        //for (int i = 0; i <numeros.length; i++) {
-            //if (numeros[i]>numero){
-                //contador++;
-            //}
-
-        //}
-        //System.out.println("El número de elementos del array que son mayores que "+
-               // " "+numero +" son: "+contador);
-
-    //}
-    //public static int mayor(int lista[]){
-        //int mayor=10;
-        //for(int i=0;i<lista.length;i++){
-            //if(lista[i]>mayor){
-                //mayor=lista[i];
-            //}
-        //}
-
-        //return mayor;
 
 

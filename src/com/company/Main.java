b2b76124da7a960488d714586bc980ecff970c34
Jose Molinos <molinossobrino.jose@gmail.com>
2020-07-21 18:04:39 +0200

    Practica 7, Algoritmo 2 Resuelto

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,24 +7,26 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         Scanner leer = new Scanner(System.in);
-        int[] numeros = new int[10];
-        System.out.printf("Introduce 10 numeros");
-        for (int i = 0; i < 10; i++) {
-            numeros[i] = leer.nextInt();
-            leer.nextLine();
-        }
-        System.out.println("Introduce numero a buscar");
-        int numero = leer.nextInt();
-        leer.nextLine();
-        int contador = 0;
-        for (int i = 0; i < numeros.length; i++) {
-            if (numeros[i] > numero) {
-                contador++;
-            }
+        int[] numeros = new int[5];
+        System.out.printf("Introduce 5 numeros positivos");
+        int num, contador = 0;
+        do {
+            do {
+                System.out.println("introducir un numero");
+                num = leer.nextInt();
+                leer.nextLine();
+            } while (num < 0);
+            numeros[contador] = num;
+            contador++;
+
+
+        } while (contador < 5);
+        System.out.println("numeros introducidos: ");
+        for (int i = 4; i >= 0; i--) {
+            System.out.println(numeros[i] + "  ");
 
         }
-        System.out.println("El numero de elementos del array que son mayores que " +
-                " " + numero + " son: " + contador);
+
+        System.out.println("");
     }
-
 }

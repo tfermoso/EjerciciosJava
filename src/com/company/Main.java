c4dfba05e1a53371c78f8e6186a138c47f8457b0
Tomas <tomas.fermoso@gmail.com>
2020-07-21 17:09:42 +0200

    practica 7 algoritmo 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,29 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner leer=new Scanner(System.in);
+        int[] numeros=new int[10];
+        System.out.println("Introduce 10 números");
+        for (int i = 0; i < 10; i++) {
+            numeros[i]=leer.nextInt();
+            leer.nextLine();
+        }
+        System.out.println("Introduce número a buscar");
+        int numero=leer.nextInt();
+        leer.nextLine();
+        int contador=0;
+        for (int i = 0; i < numeros.length; i++) {
+            if(numeros[i]>numero){
+                contador++;
+            }
+        }
+        System.out.println("El número de elementos del array que son mayores que " +
+                " "+numero +" son: "+contador);
+
     }
 }

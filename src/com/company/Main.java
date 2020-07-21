cd98e2a9699f0d383cc18024cc1acdd473214266
david <fremenida@gmail.com>
2020-07-21 18:32:28 +0200

    Preactica 7 algoritmo 3b

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,32 +7,42 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        Scanner leer=new Scanner(System.in);
-        int[] numeros = new int [20];
-        boolean resultado=false;
-        int contador=0;
-        int numero;
-        do {
-            System.out.println("Introduzca numero");
-            numero = leer.nextInt();
-            leer.nextLine();
-            numeros[contador]=numero;
-            contador++;
-            int suma=0;
-            for (int i = 0; i <numeros.length ; i++) {
-                suma+=numeros[i];
-                if (suma>=100){
-                    resultado=true;
-                }
+        Scanner leer = new Scanner(System.in);
+        int[] numeros = new int[20];
+        //boolean resultado=false;
+        //int contador=0;
+        int num, suma = 0;
+        //do {
+        for (int i = 0; i < 20; i++) {
+
+            //numeros[contador]=numero;
+            //contador++;
+            //suma+=num;
+            //int suma=0;
+            //for (int i = 0; i <numeros.length ; i++) {
+            //suma+=numeros[i];
+            if (suma >= 100) {
+                //resultado=true;
+                numeros[i] = 0;
+            } else {
+                System.out.println("Introduce numero: ");
+                num = leer.nextInt();
+                leer.nextLine();
+                suma+=num;
+                numeros[i] = num;
             }
-        }while (!resultado);
-        for (int i = 0; i <numeros.length ; i++) {
-            if (numeros[i]!=0){
-                System.out.println(numeros[i]);
-            }
+        }//while (!resultado);
+        for (int i = 0; i < 20; i++) {
+            System.out.println(numeros[i] + ",");
         }
+        //if (numeros[i]!=0){
+
     }
 }
+
+
+
+
 //        int[] numeros=new int[5];
 //        System.out.println("Introduce 5 números positivos");
 //        int contador=0,num;

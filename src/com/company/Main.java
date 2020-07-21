76c28ce7a176ec2960f328401e3ee5538d5aafe6
Fani-19 <estefbd@hotmail.com>
2020-07-21 17:18:13 +0200

    Ejercicio pedir 10 num y encontrar mayores

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,29 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner leer = new Scanner(System.in);
+        int [] num=new int[10];
+        System.out.println("Introduce 10 números: ");
+        for (int i = 0; i < 10; i++) {
+
+            num[i] = leer.nextInt();
+            leer.nextLine();
+        }
+        System.out.println("Numero a comparar: ");
+        int numero=leer.nextInt();
+        leer.nextLine();
+        int contador=0;
+        leer.nextLine();
+        for (int i = 0; i < num.length   ; i++) {
+            if(num[i]>numero){
+                contador++;
+            }
+
+        }
+        System.out.println("Los números del array que son mayores que: " + numero + "son: " + +contador);
     }
 }

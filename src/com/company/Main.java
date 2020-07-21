34ede72734cf096ec25ef17e7b5e4403a9e9d586
Jose Molinos <molinossobrino.jose@gmail.com>
2020-07-21 19:46:25 +0200

    Practica 7, Algoritmo 4 Resuelto

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,33 +3,41 @@ package com.company;
 import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 import org.w3c.dom.ls.LSOutput;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        Scanner leer = new Scanner(System.in);
-        int[] numeros = new int[20];
-        int num, suma = 0;
-        for (int i = 0; i < 20; i++) {
-            if (suma > 100) {
-                numeros[i] = 0;
-            } else {
-                System.out.println("Introduce un número");
-                num = leer.nextInt();
-                leer.nextLine();
-                suma += num;
-                numeros[i] = num;
+
+        int[] loteria = new int[6];
+        int contador = 0;
+        do {
+            //Generar numero aleatorio
+            Random rand = new Random();
+            int numero = rand.nextInt(50);
+
+            //Comprueba si ya lo tengo
+            if (!comprobarArray(numero, loteria)) {
+                loteria[contador] = numero;
+                contador++;
             }
+
+        } while (contador < 6);
+        for (int i = 0; i < loteria.length; i++) {
+            System.out.println(loteria[i] + " ");
         }
-        for (int i = 0; i < 20; i++) {
-            System.out.print(numeros[i] + " , ");
-
-        }
-
-        System.out.println("");
-
+        System.out.println(" ");
     }
 
+    private static boolean comprobarArray(int numero, int[] loteria) {
+        for (int i = 0; i < loteria.length; i++) {
+            if (loteria[i] == numero) return true;
+
+        }
+        return false;
+    }
 }

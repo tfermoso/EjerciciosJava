9f01f3de3776654256177db6434ca675412b41e2
Santi <sjamaldeite@gmail.com>
2020-07-21 20:06:07 +0200

    Temperaturas,ciudades y medias(Ejercicio 8)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,35 +5,38 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        // Realizar un algortimo que genere una combinación de la lotería primitiva. El objetivo es
-        // generar 6 números aleatorios entre 1 y 49 que no se repitan. Puedes utilizar la
-        // función GenerarAleatorio utilizada en los ejemplos del manual
-        int contador=0;
-        int[] loteria=new int[6];
-        Random random = new Random();
-        do {
-            int upperbound = 49;
-            int numero = random.nextInt(upperbound);
-            if (numero != 0) {
-                if (comprobarArray(numero, loteria, contador) == true) {
-                    contador++;
-                }
-            }
-        }while (contador<6);
-        for (int i = 0; i <loteria.length ; i++) {
-            System.out.println(loteria[i]);
+       //Realizar un algoritmo para un programa que lea el nombre y temperatura de
+        // una ciudad y los guarde en un array, hasta un total de 10 ciudades.
+        // A continuación, nos mostrará el nombre y temperatura de todas las ciudades
+        // cuya temperatura sea inferior a la media. Podemos usar una función ConvertirEntero,
+        // que nos convierte una cadena formada por caracteres numéricos a entero.
+
+        String[][] temperaturas=new String[10][2];
+        Scanner leer= new Scanner(System.in);
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("Nombre de la ciudad: ");
+            String ciudad=leer.nextLine();
+            System.out.println("Temperatura: ");
+            String temp=leer.nextLine();
+            temperaturas[i][0]=ciudad;
+            temperaturas[i][1]=temp;
         }
+        double media=0.0;
+        for (int i = 0; i <temperaturas.length ; i++) {
+            media+=convertirInt(temperaturas[i][1]);
+        }
+        media= media/10;
+        System.out.println("La media de temperatura es "+media);
+        System.out.println("Las ciudades que menor temperatura a la media son : ");
+        for (int i = 0; i <temperaturas.length ; i++) {
+            if (media>convertirInt(temperaturas[i][1])){
+                System.out.println(temperaturas[i][0] + " con temperatura "+temperaturas[i][1]);
+            }
+        }
+
     }
 
-    private static boolean comprobarArray(int numero, int[] loteria,int contador) {
-        boolean presente=false;
-        for (int i = 0; i <loteria.length ; i++) {
-            if (numero==loteria[i]&&numero!=0){
-                presente=true;
-            }
-        } if (presente!=true){
-            loteria[contador]=numero;
-        }
-        return presente;
+    public static int convertirInt(String valor){
+        return Integer.parseInt(valor);
     }
 }

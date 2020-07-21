ee58081058bd7d37de30757d0a7b4aaddfbe4205
david <fremenida@gmail.com>
2020-07-21 18:10:23 +0200

    Preactica 7 algoritmo 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -7,25 +8,50 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         Scanner leer=new Scanner(System.in);
-        int[] numeros=new int[5];
-        System.out.println("Introduce 5 números positivos");
-        int contador=0,num;
-        do{
-            System.out.println("Introduce un número");
-            num=leer.nextInt();
+        int[] numeros = new int [20];
+        boolean resultado=false;
+        int contador=0;
+        int numero;
+        do {
+            System.out.println("Introduzca numero");
+            numero = leer.nextInt();
             leer.nextLine();
-            if(num>0){
-                numeros[contador]=num;
-                contador++;
+            numeros[contador]=numero;
+            contador++;
+            int suma=0;
+            for (int i = 0; i <numeros.length ; i++) {
+                suma+=numeros[i];
+                if (suma>=100){
+                    resultado=true;
+                }
             }
-        }while(contador<5);
-        System.out.println("numeros introducidos: ");
-        for (int i = 4; i >=0 ; i--) {
-            System.out.print(numeros[i]+",");
+        }while (!resultado);
+        for (int i = 0; i <numeros.length ; i++) {
+            if (numeros[i]!=0){
+                System.out.println(numeros[i]);
+            }
         }
-        System.out.println("");
     }
 }
+//        int[] numeros=new int[5];
+//        System.out.println("Introduce 5 números positivos");
+//        int contador=0,num;
+//        do{
+//            System.out.println("Introduce un número");
+//            num=leer.nextInt();
+//            leer.nextLine();
+//            if(num>0){
+//                numeros[contador]=num;
+//                contador++;
+//            }
+//        }while(contador<5);
+//        System.out.println("numeros introducidos: ");
+//        for (int i = 4; i >=0 ; i--) {
+//            System.out.print(numeros[i]+",");
+//        }
+//        System.out.println("");
+//    }
+
             //if (num<0) {
                 //numeros[contador]=num;
             //}else {

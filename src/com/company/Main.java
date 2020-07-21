3a59991deca7a9ca789cb8520732a60d1581f3ab
Jose Molinos <molinossobrino.jose@gmail.com>
2020-07-21 17:14:12 +0200

    Practica 7, Algoritmo 1 Resuelto

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        Scanner leer = new Scanner(System.in);
+        int[] numeros = new int[10];
+        System.out.printf("Introduce 10 numeros");
+        for (int i = 0; i < 10; i++) {
+            numeros[i] = leer.nextInt();
+            leer.nextLine();
+        }
+        System.out.println("Introduce numero a buscar");
+        int numero = leer.nextInt();
+        leer.nextLine();
+        int contador = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] > numero) {
+                contador++;
+            }
+
+        }
+        System.out.println("El numero de elementos del array que son mayores que " +
+                " " + numero + " son: " + contador);
     }
+
 }

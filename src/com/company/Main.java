8ebc5f48e9ca01ca4ce9eab053605ae292faaf85
Sandra <docenciasandrag@gmail.com>
2020-07-21 17:10:32 +0200

    Created array of num and run array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner leer = new Scanner(System.in);
+        int i;
+        int[] numeros = new int[10];
+        System.out.println("Introduce 10 números");
+        for (i = 0; i < 10; i++) {
+            System.out.print("numeros[" + i + "]= ");
+            numeros[i] = leer.nextInt();
+            leer.nextLine();
+        }
+        System.out.println("Introduce número a buscar");
+        int numero = leer.nextInt();
+        leer.nextLine();
+        int contador=0;
+
+
+    //Recorrer el array
+        for( i =0;i< numeros.length; i++)
+            if (numeros[i]>numero) {
+                contador++;
+            }
+    }
+
+
     }
 }

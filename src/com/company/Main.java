fc0c3a2028e5707caa96921504c6ddc87cdd4d73
david <fremenida@gmail.com>
2020-07-21 17:34:28 +0200

    Practica 7 Algoritmo 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,26 +6,34 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Scanner leer=new Scanner(System.in);
-        int[] numeros=new int[10];//declaracion array
-        System.out.println("Introduce 10 números");
-        for (int i = 0; i <10 ; i++) {
-            numeros[i]=leer.nextInt();
-            leer.nextLine();
-
-        }
-        System.out.println("Introduce número a buscar");
-        int numero=leer.nextInt();
-        leer.nextLine();
+        int[] numeros=new int[5];
         int contador=0;
-        for (int i = 0; i <numeros.length; i++) {
-            if (numeros[i]>numero){
+        do{
+            System.out.println("Introduzca un numero");
+            Scanner leer=new Scanner(System.in);
+            int numero=leer.nextInt();
+            leer.nextLine();
+            if (numero<0) {
+
+            }else {
+                numeros[contador]=numero;
                 contador++;
             }
+        }while (contador<5);
+        System.out.println(numeros[4]+"\n"+numeros[3]+"\n"+numeros[2]+"\n"+numeros[1]+"\n"+numeros[0]);
+    }
+        //System.out.println("Introduce número a buscar");
+        //int numero=leer.nextInt();
+        //leer.nextLine();
+        //int contador=0;
+        //for (int i = 0; i <numeros.length; i++) {
+            //if (numeros[i]>numero){
+                //contador++;
+            //}
 
-        }
-        System.out.println("El número de elementos del array que son mayores que "+
-                " "+numero +" son: "+contador);
+        //}
+        //System.out.println("El número de elementos del array que son mayores que "+
+               // " "+numero +" son: "+contador);
 
     }
     //public static int mayor(int lista[]){
@@ -37,5 +45,5 @@ public class Main {
         //}
 
         //return mayor;
-    }
+
 

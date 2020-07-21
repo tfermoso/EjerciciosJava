2308155cfb0bf60a9fb422cfc0b003c7acb6e569
Sandra <docenciasandrag@gmail.com>
2020-07-21 19:07:31 +0200

    Created array with twenty  numbers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,25 +6,34 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
-        int[] numeros = new int[10];
-        int num = 0, contador = 0;
-     //llenar un array de 5 números positivos
-        do {
-            System.out.println("Introduce un número: ");
-            num = leer.nextInt();
-            leer.nextLine();
-                if (num < 0){
-                numeros[contador] = num;
-                contador++;
-             }
+        int[] numeros = new int[20];
+        int num;
+        int suma = 0;
+        //leer y llenar un array de 20 números cuya suma no supere 100
 
-        } while (contador < 5);
-        System.out.println("Números introducidos: ");
-        for (int i = 4; i >= 0 ; i--) {
-            System.out.println(numeros[i]+ ",");
+        for (int i = 0; i < 20; i++) {
+                if (suma>100){
+                    numeros[i] = 0;
+                } else{
+                    System.out.println("Introduce un número: ");
+                    num = leer.nextInt();
+                    leer.nextLine();
+                    suma += num;
+                    numeros[i] = num;
 
             }
-        System.out.println("");
+        }
+            for (int i = 0; i < 20; i++) {
+                System.out.println(numeros[i]+",");
+
+        }
+            System.out.println(numeros.toString());
+
+
+
+        }
     }
-}
+
+
+
 

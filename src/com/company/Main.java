b0a0db79c8133c341b3fd2368c74ceebafa38c89
Santi <sjamaldeite@gmail.com>
2020-07-21 17:31:28 +0200

    Array terminado al llegar o pasar 100(Ejercicio 7)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,21 +6,33 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int[] numeros=new int[5];
+    //    Tenemos un array de enteros de tamaño 20. Realizar un algoritmo que vaya leyendo números y los guarde
+        //    en el array, pero si la suma de los números leídos alcanza o supera 100, se dejarán de leer números
+        //    y rellenará con 0 las posiciones que queden libres.
+        Scanner leer= new Scanner(System.in);
+        int[] numeros = new int [20];
+        boolean acabado=false;
         int contador=0;
-        do{
-        System.out.println("Introduzca numero");
-        Scanner leer=new Scanner(System.in);
-        int numero=leer.nextInt();
-        leer.nextLine();
-        if (numero<0) {
-
-        }else {
+        int numero;
+        do {
+            System.out.println("Introduzca numero");
+            numero = leer.nextInt();
+            leer.nextLine();
             numeros[contador]=numero;
             contador++;
+            int suma=0;
+            for (int i = 0; i <numeros.length ; i++) {
+                suma+=numeros[i];
+                if (suma>=100){
+                    acabado=true;
+                }
+            }
+        }while (!acabado);
+        for (int i = 0; i <numeros.length ; i++) {
+            if (numeros[i]!=0){
+                System.out.println(numeros[i]);
+            }
         }
-        }while (contador<5);
-        System.out.println(numeros[4]+"\n"+numeros[3]+"\n"+numeros[2]+"\n"+numeros[1]+"\n"+numeros[0]);
     }
 }
 

b9cab65714e841b3622b3c651dce667b833bdfcb
Santi <sjamaldeite@gmail.com>
2020-07-21 17:53:53 +0200

    Loteria(Ejercicio 7)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,36 +2,30 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-    //    Tenemos un array de enteros de tamaño 20. Realizar un algoritmo que vaya leyendo números y los guarde
-        //    en el array, pero si la suma de los números leídos alcanza o supera 100, se dejarán de leer números
-        //    y rellenará con 0 las posiciones que queden libres.
-        Scanner leer= new Scanner(System.in);
-        int[] numeros = new int [20];
-        boolean acabado=false;
+    // Realizar un algortimo que genere una combinación de la lotería primitiva. El objetivo es
+        // generar 6 números aleatorios entre 1 y 49 que no se repitan. Puedes utilizar la
+        // función GenerarAleatorio utilizada en los ejemplos del manual
         int contador=0;
-        int numero;
+        int[] loteria=new int[6];
+        Random random = new Random();
         do {
-            System.out.println("Introduzca numero");
-            numero = leer.nextInt();
-            leer.nextLine();
-            numeros[contador]=numero;
-            contador++;
-            int suma=0;
-            for (int i = 0; i <numeros.length ; i++) {
-                suma+=numeros[i];
-                if (suma>=100){
-                    acabado=true;
+            int upperbound = 49;
+            int numero = random.nextInt(upperbound);
+            for (int i = 0; i <loteria.length ; i++) {
+                    if (numero!=loteria[1]&&numero!=loteria[2]&&numero!=loteria[3]&&numero!=loteria[4]
+                            &&numero!=loteria[5]&&numero!=loteria[0]&& numero!=0){
+                        loteria[contador]=numero;
+                        contador++;
+                    }
                 }
-            }
-        }while (!acabado);
-        for (int i = 0; i <numeros.length ; i++) {
-            if (numeros[i]!=0){
-                System.out.println(numeros[i]);
-            }
+        }while (contador<6);
+        for (int i = 0; i <loteria.length ; i++) {
+            System.out.println(loteria[i]);
         }
     }
 }

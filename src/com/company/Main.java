adacf5c341070e4fc0d04adfbad84b5ecd862157
Fani-19 <estefbd@hotmail.com>
2020-07-22 20:12:47 +0200

    suma nº comprendidos entre 2 numeros y factorial de la suma

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,75 +6,47 @@ import java.util.Scanner;
 
 public class Main {
 
+
     public static void main(String[] args) {
 
-
-        int [] num =new int[24];
-       num=leerNUmeros(num);
-
-        //ordenar el array de numeros
-        int[] arrayOrdenado;
-        arrayOrdenado=ordenarArray(num);
-
-        //llenar la matriz
-
-
-        int [][]numerosMatriz = new int[4][6];
-    numerosMatriz=llenarMatriz(numerosMatriz,arrayOrdenado);
-
-        //mostrar la matriz
-       mostrarMatriz(numerosMatriz);
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingresa el primer valor: ");
+      int a=leer.nextInt();
+        System.out.println("Ingresa el segundo Valor: ");
+        int b=leer.nextInt();
+        int suma=calcularSumaNumeros(a,b);
+        System.out.println(suma);
+        System.out.println(factorial(suma));
     }
 
-    private static int[][] llenarMatriz(int[][] numerosMatriz, int[] arrayOrdenado) {
-        int indice=0;
-        for (int i = 0; i <numerosMatriz.length ; i++) {
-            for (int j = 0; j <numerosMatriz[0].length; j++) {
-                numerosMatriz[i][j] = arrayOrdenado[indice];
-                indice++;
+    private static int factorial(int n) {
+        int iFactorial = 1;
+        for (int i=2;i<=n;i++)
+            iFactorial = iFactorial * i;
+        return iFactorial;
+    }
+
+    private static int calcularSumaNumeros(int a, int b) {
+        int inicio = 0, fin = 0, suma=0;
+        if (a == b) {
+            System.out.println("Datos iguales.");
+        } else {
+            if (a > b) {
+                inicio = b;
+                fin = a;
+            } else {
+                inicio = a;
+                fin = b;
             }
-
-
         }
-        return numerosMatriz;
-    }
-
-    private static int[] ordenarArray(int[] num) {
-        int menor;
-        int aux;
-        for (int i = 0; i <num.length-1 ; i++) {
-            menor=i;
-            for (int j = i+1; j <num.length ; j++) {
-                if(num[j]<num[menor]){
-                    menor=j;
-                }
-
-            }
-            aux=num[i];
-            num[i]=num[menor];
-            num[menor]=aux;
+        for (int i = inicio+1; i < fin; i++) {
+            suma+=i;
         }
-        return num;
+        return suma;
+    }
     }
 
-    public static void mostrarMatriz(int[][]numerosMatriz){
-        for (int i = 0; i <numerosMatriz.length ; i++) {
-            for (int j = 0; j <numerosMatriz[0].length ; j++) {
-                System.out.print(numerosMatriz[i][j]+ "|");
 
-            }
-            System.out.println("");
-        }
-    }
-   public static int[] leerNUmeros(int[]num) {
-       Scanner leer = new Scanner(System.in);
-       for (int i = 0; i < num.length; i++) {
-           System.out.println("Escribe un numero: ");
-           num[i] = leer.nextInt();
-           leer.nextLine();
-       }
-       return num;
-   }
-    }
+
 
 

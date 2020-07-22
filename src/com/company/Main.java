1482b9aa22790b8fc6c71e079a15854606d4a7d9
Tomas <tomas.fermoso@gmail.com>
2020-07-22 20:12:05 +0200

    practica 10 algoritmo 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,70 +8,44 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-        // Leemos los 24 números
-        int[] numeros=new int[24];
-        numeros=leerNumeros(numeros);
-
-        //Ordenamos el array de números
-        int[] arrayOrdenado;
-        arrayOrdenado=ordenarArray(numeros);
-
-        //Llenar la matriz
-        int[][] matrizNumeros=new int[4][6];
-        matrizNumeros=llenarMatriz(matrizNumeros,arrayOrdenado);
-
-        //mostrar la matriz
-        mostrarMatriz(matrizNumeros);
-
-    }
-
-    private static int[][] llenarMatriz(int[][] matrizNumeros, int[] arrayOrdenado) {
-        int indice=0;
-        for (int i = 0; i < matrizNumeros.length; i++) {
-            for (int j = 0; j < matrizNumeros[0].length; j++) {
-                matrizNumeros[i][j]=arrayOrdenado[indice];
-                indice++;
-            }
-        }
-        return matrizNumeros;
-    }
-
-    private static int[] ordenarArray(int[] numeros) {
-        int minimo;
-        int aux;
-        for (int i = 0; i < numeros.length-1 ; i++) {
-            minimo=i;
-            for (int j = i+1; j < numeros.length; j++) {
-                if(numeros[j]<numeros[minimo]) {
-                    minimo = j;
-                }
-            }
-            aux=numeros[i];
-            numeros[i]=numeros[minimo];
-            numeros[minimo]=aux;
-        }
-        return numeros;
-    }
-
-    public static void mostrarMatriz(int[][] matriz){
-        //mostrar la matriz
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[0].length ; j++) {
-                System.out.print(matriz[i][j]+"|");
-            }
-            System.out.println("");
-        }
-    }
-
-    public static int[] leerNumeros(int[] numeros){
         Scanner leer=new Scanner(System.in);
-        for (int i = 0; i < numeros.length; i++) {
-            System.out.println("Introduce número: ");
-            numeros[i]=leer.nextInt();
-            leer.nextLine();
+        System.out.println("Introduce un número: ");
+        int a=leer.nextInt();
+        leer.nextLine();
+        System.out.println("Introduce un número: ");
+        int b=leer.nextInt();
+        int suma=calcularSumaNumeros(a,b);
+        System.out.println(suma);
+        System.out.println(factorial(suma));
+
+    }
+
+    private static int factorial(int n) {
+        if(n<=1) return 1;
+        return n*factorial(n-1);
+    }
+    private static int factorialIterativa(int n) {
+       int producto=1;
+       while(n>1){
+           producto=producto*n;
+           n--;
+       }
+       return producto;
+    }
+
+    private static int calcularSumaNumeros(int a, int b) {
+        int inicio,fin,suma=0;
+        if(a>b){
+            inicio=b;
+            fin=a;
+        }else{
+            inicio=a;
+            fin=b;
         }
-        return numeros;
+        for (int i = inicio+1; i < fin; i++) {
+            suma+=i;
+        }
+            return suma;
     }
 
 

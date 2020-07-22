15006760bef67eb0c4e1b73a9daaaa3bff250604
Tomas <tomas.fermoso@gmail.com>
2020-07-22 19:19:10 +0200

    practica 9 algoritmo 2 programación modular

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,17 +9,35 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         // Leemos los 24 números
         int[] numeros=new int[24];
-        Scanner leer=new Scanner(System.in);
-        for (int i = 0; i < 24; i++) {
-            System.out.println("Introduce número: ");
-            numeros[i]=leer.nextInt();
-            leer.nextLine();
-        }
+        numeros=leerNumeros(numeros);
 
         //Ordenamos el array de números
+        int[] arrayOrdenado;
+        arrayOrdenado=ordenarArray(numeros);
+
+        //Llenar la matriz
+        int[][] matrizNumeros=new int[4][6];
+        matrizNumeros=llenarMatriz(matrizNumeros,arrayOrdenado);
+
+        //mostrar la matriz
+        mostrarMatriz(matrizNumeros);
+
+    }
+
+    private static int[][] llenarMatriz(int[][] matrizNumeros, int[] arrayOrdenado) {
+        int indice=0;
+        for (int i = 0; i < matrizNumeros.length; i++) {
+            for (int j = 0; j < matrizNumeros[0].length; j++) {
+                matrizNumeros[i][j]=arrayOrdenado[indice];
+                indice++;
+            }
+        }
+        return matrizNumeros;
+    }
+
+    private static int[] ordenarArray(int[] numeros) {
         int minimo;
         int aux;
         for (int i = 0; i < numeros.length-1 ; i++) {
@@ -33,25 +51,27 @@ public class Main {
             numeros[i]=numeros[minimo];
             numeros[minimo]=aux;
         }
+        return numeros;
+    }
 
-        //Llenar la matriz
-        int indice=0;
-        int[][] matrizNumeros=new int[4][6];
-        for (int i = 0; i < matrizNumeros.length; i++) {
-            for (int j = 0; j < matrizNumeros[0].length; j++) {
-                matrizNumeros[i][j]=numeros[indice];
-                indice++;
-            }
-        }
-
+    public static void mostrarMatriz(int[][] matriz){
         //mostrar la matriz
-        for (int i = 0; i < matrizNumeros.length; i++) {
-            for (int j = 0; j < matrizNumeros[0].length ; j++) {
-                System.out.print(matrizNumeros[i][j]+"|");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length ; j++) {
+                System.out.print(matriz[i][j]+"|");
             }
             System.out.println("");
         }
+    }
 
+    public static int[] leerNumeros(int[] numeros){
+        Scanner leer=new Scanner(System.in);
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Introduce número: ");
+            numeros[i]=leer.nextInt();
+            leer.nextLine();
+        }
+        return numeros;
     }
 
 

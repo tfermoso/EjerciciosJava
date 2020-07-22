80d4d937ae65b42782060bd63b1e2224ba1b7d30
Santi <sjamaldeite@gmail.com>
2020-07-22 20:11:47 +0200

    Factorial (Ejercicio 10)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,68 +5,52 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-       //Dado un array de dos dimensiones de 4x6, realizar un programa que lea 24 números
-        // y los coloque en el array. Posteriormente, se ordenará el array de menor a mayor
-        // (la posición 1,1 tendrá el número más pequeño, mientras que 4,6 tendrá el mayor) y se mostrará ordenado
-        int[][] numeros= new int[4][6];
-        int[] numerosAux=new int[24];
-        numerosAux=meterNumeros(numerosAux);
-        ordenarNumeros(numerosAux);
-        meterNumerosMatriz(numeros,numerosAux);
-        pintarMatriz(numeros);
+       //dar 2 numeros. Dar la suma de los numeros entre medias
+        Scanner leer=new Scanner(System.in);
+        int numero1=leerNumero();
+        int numero2=leerNumero();
+        int suma =devolverSuma(numero1,numero2);
+        System.out.println(suma);
+        int factorial=factorialNumero(suma);
+        String factorialSinCalcular=factorialSinCalcular(suma);
+        System.out.println(factorial);
+        System.out.println(factorialSinCalcular);
     }
 
-    private static void pintarMatriz(int[][] numeros) {
-        System.out.println("Array ordenada: ");
-        for (int i = 0; i <numeros[0].length ; i++) {
-            for (int j = 0; j <numeros.length ; j++) {
-                System.out.print(numeros[j][i]);
-                if (j<3){
-                    System.out.print(",");
-                }else if(j==3){
-                    System.out.print("|");
-                }
-
-            }System.out.println("");
+    private static String factorialSinCalcular(int suma) {
+        String factorial=String.valueOf(suma);
+        for (int i = 1; i < suma; i++) {
+            factorial+="·"+String.valueOf(suma-i);
         }
+        return factorial;
     }
 
-    private static void meterNumerosMatriz(int[][] numeros, int[] numerosAux) {
-        int contadorH=0;
-        int contadorV=0;
-        for (int i = 0; i <numerosAux.length ; i++) {
-            numeros[contadorH][contadorV]=numerosAux[i];
-            contadorH++;
-            if (contadorH==4){
-                contadorV++;
-                contadorH=0;
+    private static int factorialNumero(int suma) {
+        int factorial=1;
+        for (int i = 2; i <=suma ; i++) {
+            factorial= factorial*i;
             }
-        }
+        return factorial;
     }
 
-    private static void ordenarNumeros(int[] numerosAux) {
-        int minimo;
-        int numAux;
-        for (int i = 0; i <numerosAux.length-1 ; i++) {
-            minimo=i;
-            for (int j = i+1; j <numerosAux.length ; j++) {
-                if (numerosAux[j]<numerosAux[minimo]){
-                    minimo=j;
-                }
-            }
-            numAux=numerosAux[i];
-            numerosAux[i]=numerosAux[minimo];
-            numerosAux[minimo]=numAux;
-        }
+    private static int devolverSuma(int numero1, int numero2) {
+       int suma=0;
+       if (numero1<numero2) {
+           for (int i = numero1+1; i <numero2; i++) {
+                suma+=i;
+           }
+       }else if (numero2<numero1){
+           for (int i = numero2+1; i <numero1 ; i++) {
+               suma+=i;
+           }
+       }
+       return suma;
     }
 
-    private static int[] meterNumeros(int[] numerosAux) {
-        Scanner leer= new Scanner(System.in);
-        for (int i = 0; i <24 ; i++) {
-            System.out.println("Introduzca numero");
-            int numero=leer.nextInt();
-            leer.nextLine();
-            numerosAux[i]=numero;
-        } return numerosAux;
+    private static int leerNumero() {
+        Scanner leer=new Scanner(System.in);
+        System.out.println("Introduzca numero");
+        int numer=leer.nextInt();
+        return numer;
     }
 }

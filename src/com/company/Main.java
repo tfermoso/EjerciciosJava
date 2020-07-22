d694930a1d921c5713fc0739287b967dddf479ca
Sergio <sergioval16@outlook.com>
2020-07-22 20:19:45 +0200

    calcular el factorial

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,78 +1,52 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        //leemos 24 numeros
-        int[] numeros = new int[24];
-        numeros = leerNumeros(numeros);
-
-        //ordenamos el array de numeros
-        int[] arrayOrdenado;
-        arrayOrdenado = ordenarArray(numeros);
-
-        //llenar la matriz
-        int[][] matrizNumeros = new int[4][6];
-        matrizNumeros = llenarMatriz(matrizNumeros, arrayOrdenado);
-
-        //mostrar matriz
-        mostrarMatriz(matrizNumeros); //llama al metodo mostrar matriz
+        Scanner leer=new Scanner(System.in);
+        System.out.println("Introduce un número: ");
+        int a=leer.nextInt();
+        leer.nextLine();
+        System.out.println("Introduce un número: ");
+        int b=leer.nextInt();
+        int suma=calcularSumaNumeros(a,b);
+        System.out.println(suma);
+        System.out.println(factorial(suma));
 
     }
 
-    private static int[][] llenarMatriz(int[][] matrizNumeros, int[] arrayOrdenado) {
-        int indice = 0;
-        for (int i = 0; i < matrizNumeros.length; i++)
-            for (int j = 0; j < matrizNumeros[0].length; j++) {
-                matrizNumeros[i][j] = arrayOrdenado[indice];
-                indice++;
-            }
-        return matrizNumeros;
+    private static int factorial(int n) {
+        if(n<=1) return 1;
+        return n*factorial(n-1);
     }
-
-
-    private static int[] ordenarArray(int[] numeros) {
-        int minimo;
-        int aux;
-        for (int i = 0; i < numeros.length - 1; i++) {
-            minimo = i;
-            for (int j = i + 1; j < numeros.length; j++) {
-                if (numeros[j] < numeros[minimo]) {
-                    minimo = j;
-                }
-            }
-            aux = numeros[i];
-            numeros[i] = numeros[minimo];
-            numeros[minimo] = aux;
+    private static int factorialIterativa(int n) {
+        int producto=1;
+        while(n>1){
+            producto=producto*n;
+            n--;
         }
-        return numeros;
-
+        return producto;
     }
 
-    public static void mostrarMatriz(int[][] matriz) {
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[0].length; j++) {
-                System.out.print(matriz[i][j] + "/");
-            }
-            System.out.println("");
+    private static int calcularSumaNumeros(int a, int b) {
+        int inicio,fin,suma=0;
+        if(a>b){
+            inicio=b;
+            fin=a;
+        }else{
+            inicio=a;
+            fin=b;
         }
-    }
-
-    public static int[] leerNumeros(int[] numeros) {
-        Scanner leer = new Scanner(System.in);
-        for (int i = 0; i < numeros.length; i++) {
-            System.out.println("Introduce numero: ");
-            numeros[i] = leer.nextInt();
-            leer.nextLine();
-
-
+        for (int i = inicio+1; i < fin; i++) {
+            suma+=i;
         }
-        return numeros;
+        return suma;
     }
 
 
 }
-

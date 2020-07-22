b7a3d7bab4c38d714bffbefff4a40dcf0ccb4042
Santi <sjamaldeite@gmail.com>
2020-07-22 18:29:22 +0200

    Ordenar matriz numeros *MODULAR* (Ejercicio 10)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,13 +10,41 @@ public class Main {
         // (la posición 1,1 tendrá el número más pequeño, mientras que 4,6 tendrá el mayor) y se mostrará ordenado
         int[][] numeros= new int[4][6];
         int[] numerosAux=new int[24];
-        Scanner leer= new Scanner(System.in);
-        for (int i = 0; i <24 ; i++) {
-            System.out.println("Introduzca numero");
-            int numero=leer.nextInt();
-            leer.nextLine();
-           numerosAux[i]=numero;
+        numerosAux=meterNumeros(numerosAux);
+        ordenarNumeros(numerosAux);
+        meterNumerosMatriz(numeros,numerosAux);
+        pintarMatriz(numeros);
+    }
+
+    private static void pintarMatriz(int[][] numeros) {
+        System.out.println("Array ordenada: ");
+        for (int i = 0; i <numeros[0].length ; i++) {
+            for (int j = 0; j <numeros.length ; j++) {
+                System.out.print(numeros[j][i]);
+                if (j<3){
+                    System.out.print(",");
+                }else if(j==3){
+                    System.out.print("|");
+                }
+
+            }System.out.println("");
         }
+    }
+
+    private static void meterNumerosMatriz(int[][] numeros, int[] numerosAux) {
+        int contadorH=0;
+        int contadorV=0;
+        for (int i = 0; i <numerosAux.length ; i++) {
+            numeros[contadorH][contadorV]=numerosAux[i];
+            contadorH++;
+            if (contadorH==4){
+                contadorV++;
+                contadorH=0;
+            }
+        }
+    }
+
+    private static void ordenarNumeros(int[] numerosAux) {
         int minimo;
         int numAux;
         for (int i = 0; i <numerosAux.length-1 ; i++) {
@@ -30,23 +58,15 @@ public class Main {
             numerosAux[i]=numerosAux[minimo];
             numerosAux[minimo]=numAux;
         }
+    }
 
-        int contadorH=0;
-        int contadorV=0;
-        for (int i = 0; i <numerosAux.length ; i++) {
-            numeros[contadorH][contadorV]=numerosAux[i];
-            contadorH++;
-            if (contadorH==4){
-                contadorV++;
-                contadorH=0;
-            }
-        }
-        System.out.println("Array ordenada: ");
-        for (int i = 0; i <6 ; i++) {
-            for (int j = 0; j <4 ; j++) {
-                System.out.print(numeros[j][i]+",");
-
-            }System.out.println("");
-        }
+    private static int[] meterNumeros(int[] numerosAux) {
+        Scanner leer= new Scanner(System.in);
+        for (int i = 0; i <24 ; i++) {
+            System.out.println("Introduzca numero");
+            int numero=leer.nextInt();
+            leer.nextLine();
+            numerosAux[i]=numero;
+        } return numerosAux;
     }
 }

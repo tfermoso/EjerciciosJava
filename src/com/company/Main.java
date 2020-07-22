665cd7c261c059f2c0ef6fb35699ab7bdac53965
Sandra <docenciasandrag@gmail.com>
2020-07-22 17:13:09 +0200

    practica 9 algoritmo 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,38 +1,55 @@
 package com.company;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        String[] textos=new String[10];
-        Scanner leer=new Scanner(System.in);
+
+
+        String[] textos = new String[10];
+        Scanner leer = new Scanner(System.in);
         for (int i = 0; i < 10; i++) {
             System.out.println("Introduce texto: ");
-            textos[i]=leer.nextLine();
+            textos[i] = leer.nextLine();
 
         }
         int minimo;
         String aux;
-        for (int i = 0; i < textos.length-1 ; i++) {
-            minimo=i;
-            for (int j = i+1; j < textos.length; j++) {
-                if (textos[j].length()<textos[minimo].length()){
-                    minimo=j;
-                }
+        for (int i = 0; i < textos.length - 1; i++) {
+            minimo = i;
+            for (int j = i + 1; j < textos.length; j++) {
+                if (textos[j].length() < textos[minimo].length()) {
+                    minimo = j;
+                } else if (textos[j].length() == textos[minimo].length()) {
+                    if (numeroVocales(textos[j]) > numeroVocales(textos[minimo])) {
+                        minimo = j;
+                    }
 
+                }
+                aux = textos[i];
+                textos[i] = textos[minimo];
+                textos[minimo] = aux;
             }
-            aux=textos[i];
-            textos[i]=textos[minimo];
-            textos[minimo]=aux;
         }
         for (int i = 0; i < textos.length; i++) {
             System.out.println(textos[i]);
 
         }
-
     }
+        public static int numeroVocales(String palabra){
+            int vocales = 0;
+            for (int x = 0; x < palabra.length(); x++) {
+                char letraActual = palabra.charAt(x);
+                if (esVocal(letraActual)) vocales++;
+            }
+            return vocales;
+        }
+    private static boolean esVocal(char letra) {
+        return "aeiouáéíóúäëïöü".contains(String.valueOf(letra).toLowerCase());
+    }
+
+
 }
 
 

3b775f5360e68856fdeed0d3f8c2ac3a2777a783
david <fremenida@gmail.com>
2020-07-22 19:52:31 +0200

    Algoritmo 2 Ejercicio 10 Programacion Modular

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,71 +8,37 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-        // Leemos los 24 números
-        int[] numeros=new int[24];
-        numeros=leerNumeros(numeros);
-
-        //Ordenamos el array de números
-        int[] arrayOrdenado;
-        arrayOrdenado=ordenarArray(numeros);
-
-        //Llenar la matriz
-        int[][] matrizNumeros=new int[4][6];
-        matrizNumeros=llenarMatriz(matrizNumeros,arrayOrdenado);
-
-        //mostrar la matriz
-        mostrarMatriz(matrizNumeros);
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Introduce un número: ");
+        int a = leer.nextInt();
+        leer.nextLine();
+        System.out.println("Introduce un número: ");
+        int b = leer.nextInt();
+        leer.nextLine();
+        int suma = calcularSumaNumeros(a, b);
+        System.out.println(suma);
 
     }
 
-    private static int[][] llenarMatriz(int[][] matrizNumeros, int[] arrayOrdenado) {
-        int indice=0;
-        for (int i = 0; i < matrizNumeros.length; i++) {
-            for (int j = 0; j < matrizNumeros[0].length; j++) {
-                matrizNumeros[i][j]=arrayOrdenado[indice];
-                indice++;
-            }
+    private static int calcularSumaNumeros(int a, int b) {
+        int inicio, fin, suma = 0;
+        if (a > b) {
+            inicio = b;
+            fin = a;
+        } else {
+            inicio=a;
+            fin=b;
         }
-        return matrizNumeros;
-    }
+        for (int i = 0; i < inicio+1 ; i++) {
+            suma+=1;
 
-    private static int[] ordenarArray(int[] numeros) {
-        int minimo;
-        int aux;
-        for (int i = 0; i < numeros.length-1 ; i++) {
-            minimo=i;
-            for (int j = i+1; j < numeros.length; j++) {
-                if(numeros[j]<numeros[minimo]) {
-                    minimo = j;
-                }
-            }
-            aux=numeros[i];
-            numeros[i]=numeros[minimo];
-            numeros[minimo]=aux;
         }
-        return numeros;
-    }
+        return suma;
 
-    public static void mostrarMatriz(int[][] matriz){
-        //mostrar la matriz
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[0].length ; j++) {
-                System.out.print(matriz[i][j]+"|");
-            }
-            System.out.println("");
         }
     }
 
-    public static int[] leerNumeros(int[] numeros){
-        Scanner leer=new Scanner(System.in);
-        for (int i = 0; i < numeros.length; i++) {
-            System.out.println("Introduce número: ");
-            numeros[i]=leer.nextInt();
-            leer.nextLine();
-        }
-        return numeros;
-    }
 
 
-}
+
+

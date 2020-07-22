0656266ab95c1c393b03006bcf9073030f74fe38
Santi <sjamaldeite@gmail.com>
2020-07-22 16:50:22 +0200

    Contar letras(Ejercicio 9)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,38 +5,44 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-       //Realizar un algoritmo para un programa que lea el nombre y temperatura de
-        // una ciudad y los guarde en un array, hasta un total de 10 ciudades.
-        // A continuación, nos mostrará el nombre y temperatura de todas las ciudades
-        // cuya temperatura sea inferior a la media. Podemos usar una función ConvertirEntero,
-        // que nos convierte una cadena formada por caracteres numéricos a entero.
-
-        String[][] temperaturas=new String[10][2];
-        Scanner leer= new Scanner(System.in);
+       //Realizar un algoritmo para un programa que se encargue de leer 10 textos y,
+        // posteriormente, nos muestre los textos ordenados de menor a mayor número de caracteres.
+        // En caso de que dos textos tengan el mismo número de caracteres, se pondrá primero el que tenga
+        // mayor número de vocales. Para este algoritmo podemos utilizar las funciones Longitud y ObtenerLetra.
+        Scanner leer=new Scanner(System.in);
+        String[] textos=new String [10];
         for (int i = 0; i <10 ; i++) {
-            System.out.println("Nombre de la ciudad: ");
-            String ciudad=leer.nextLine();
-            System.out.println("Temperatura: ");
-            String temp=leer.nextLine();
-            temperaturas[i][0]=ciudad;
-            temperaturas[i][1]=temp;
+            System.out.println("Mete texto");
+            textos[i]=leer.nextLine();
         }
-        double media=0.0;
-        for (int i = 0; i <temperaturas.length ; i++) {
-            media+=convertirInt(temperaturas[i][1]);
-        }
-        media= media/10;
-        System.out.println("La media de temperatura es "+media);
-        System.out.println("Las ciudades que menor temperatura a la media son : ");
-        for (int i = 0; i <temperaturas.length ; i++) {
-            if (media>convertirInt(temperaturas[i][1])){
-                System.out.println(temperaturas[i][0] + " con temperatura "+temperaturas[i][1]);
+        int minimo;
+        String aux;
+        for (int i = 0; i <textos.length-1 ; i++) {
+            minimo=i;
+            for (int j = i+1; j <textos.length ; j++) {
+                if (textos[j].length()<textos[minimo].length()){
+                    minimo=j;
+                }else if(contarVocales(textos[j])<contarVocales(textos[minimo])){
+                    minimo=j;
+                }
             }
+        aux=textos[i];
+            textos[i]=textos[minimo];
+            textos[minimo]=aux;
         }
-
+        for (int i = 0; i <textos.length ; i++) {
+            System.out.println(textos[i]);
+        }
     }
-
-    public static int convertirInt(String valor){
-        return Integer.parseInt(valor);
+    public static int contarVocales(String txt){
+        int contar=0;
+        String vocales="aáAÁeéEÉiíIÍoóOÓuúüUÚÜ";
+        for (int i = 0; i <txt.length() ; i++) {
+            for (int j = 0; j < vocales.length(); j++) {
+                if (txt.charAt(i)==vocales.charAt(j)){
+                    contar++;
+                }
+            }
+        }return contar;
     }
 }

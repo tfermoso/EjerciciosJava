7519bbd3464266d69bc1e34131c5a7331c4c29f2
Santi <santialcantaramartin@gmail.com>
2020-07-22 18:10:43 +0200

    Ordenar matriz numeros(Ejercicio 9)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,47 +5,48 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-       //Realizar un algoritmo para un programa que se encargue de leer 10 textos y,
-        // posteriormente, nos muestre los textos ordenados de menor a mayor número de caracteres.
-        // En caso de que dos textos tengan el mismo número de caracteres, se pondrá primero el que tenga
-        // mayor número de vocales. Para este algoritmo podemos utilizar las funciones Longitud y ObtenerLetra.
-        Scanner leer=new Scanner(System.in);
-        String[] textos=new String [10];
-        for (int i = 0; i <10 ; i++) {
-            System.out.println("Mete texto");
-            textos[i]=leer.nextLine();
+       //Dado un array de dos dimensiones de 4x6, realizar un programa que lea 24 números
+        // y los coloque en el array. Posteriormente, se ordenará el array de menor a mayor
+        // (la posición 1,1 tendrá el número más pequeño, mientras que 4,6 tendrá el mayor) y se mostrará ordenado
+        int[][] numeros= new int[4][6];
+        int[] numerosAux=new int[24];
+        Scanner leer= new Scanner(System.in);
+        for (int i = 0; i <24 ; i++) {
+            System.out.println("Introduzca numero");
+            int numero=leer.nextInt();
+            leer.nextLine();
+           numerosAux[i]=numero;
         }
         int minimo;
-        String aux;
-        for (int i = 0; i <textos.length-1 ; i++) {
+        int numAux;
+        for (int i = 0; i <numerosAux.length-1 ; i++) {
             minimo=i;
-            for (int j = i+1; j <textos.length ; j++) {
-                if (textos[j].length() == textos[minimo].length()) {
-                    if (contarVocales(textos[j]) > contarVocales(textos[minimo])) {
-                        minimo = j;
-                    }
-                }
-                else if (textos[j].length() <= textos[minimo].length()) {
-                    minimo = j;
+            for (int j = i+1; j <numerosAux.length ; j++) {
+                if (numerosAux[j]<numerosAux[minimo]){
+                    minimo=j;
                 }
             }
-        aux=textos[i];
-            textos[i]=textos[minimo];
-            textos[minimo]=aux;
+            numAux=numerosAux[i];
+            numerosAux[i]=numerosAux[minimo];
+            numerosAux[minimo]=numAux;
         }
-        for (int i = 0; i <textos.length ; i++) {
-            System.out.println(textos[i]);
-        }
-    }
-    public static int contarVocales(String txt){
-        int contar=0;
-        String vocales="aáAÁeéEÉiíIÍoóOÓuúüUÚÜ";
-        for (int i = 0; i <txt.length() ; i++) {
-            for (int j = 0; j < vocales.length(); j++) {
-                if (txt.charAt(i)==vocales.charAt(j)){
-                    contar++;
-                }
+
+        int contadorH=0;
+        int contadorV=0;
+        for (int i = 0; i <numerosAux.length ; i++) {
+            numeros[contadorH][contadorV]=numerosAux[i];
+            contadorH++;
+            if (contadorH==4){
+                contadorV++;
+                contadorH=0;
             }
-        }return contar;
+        }
+        System.out.println("Array ordenada: ");
+        for (int i = 0; i <6 ; i++) {
+            for (int j = 0; j <4 ; j++) {
+                System.out.print(numeros[j][i]+",");
+
+            }System.out.println("");
+        }
     }
 }

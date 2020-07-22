4ac5f2d511700930b5a5bfcbd1329583f07d9d87
david <fremenida@gmail.com>
2020-07-22 20:45:21 +0200

    Algoritmo 2 Ejercicio 10 Clase Pila

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,60 +1,19 @@
 package com.company;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner leer = new Scanner(System.in);
-        System.out.println("Introduce un número: ");
-        int a = leer.nextInt();
-        leer.nextLine();
-        System.out.println("Introduce un número: ");
-        int b = leer.nextInt();
-        leer.nextLine();
-        int suma = calcularSumaNumeros(a, b);
-        System.out.println(suma);
-        System.out.println(factorial(suma));
-
-    }
-
-    private static int factorial(int n) {//Recursiva
-        if (n<=1) return 1;
-        return n*factorial(n-1);
-
-    }
-    private static int factorialIterativo(int n){//No recursivo
-        int producto=1;
-        while (n>1){
-            producto=producto*n;
-            n--;
-        }
-        return producto;
-    }
-
-    private static int calcularSumaNumeros(int a, int b) {
-        int inicio, fin, suma = 0;
-        if (a > b) {
-            inicio = b;
-            fin = a;
-        } else {
-            inicio=a;
-            fin=b;
-        }
-        for (int i = 0; i < inicio+1 ; i++) {
-            suma+=1;
-
-        }
-        return suma;
-
-        }
-
+        Stack<String>pila=new Stack<>();
+        pila.push("hola");
+        pila.push("mundo");
+        System.out.println(pila.pop());
+        System.out.println(pila.pop());
 
 
     }
+}
 
 
 

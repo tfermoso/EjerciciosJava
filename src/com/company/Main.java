aeb2c35b65a3c97e2f1ed33334df49c3db51bad0
Tomas <tomas.fermoso@gmail.com>
2020-07-23 20:13:46 +0200

    Práctica 12 algoritmo 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,52 +1,18 @@
 package com.company;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner leer=new Scanner(System.in);
-        System.out.println("Introduce un número: ");
-        int a=leer.nextInt();
-        leer.nextLine();
-        System.out.println("Introduce un número: ");
-        int b=leer.nextInt();
-        int suma=calcularSumaNumeros(a,b);
-        System.out.println(suma);
-        System.out.println(factorial(suma));
+        Pila pila = new Pila("FIFO");
+        pila.push("hola");
+        pila.push("mundo");
+        System.out.println(pila.pop());
+        System.out.println(pila.pop());
 
     }
 
-    private static int factorial(int n) {
-        if(n<=1) return 1;
-        return n*factorial(n-1);
-    }
-    private static int factorialIterativa(int n) {
-       int producto=1;
-       while(n>1){
-           producto=producto*n;
-           n--;
-       }
-       return producto;
-    }
-
-    private static int calcularSumaNumeros(int a, int b) {
-        int inicio,fin,suma=0;
-        if(a>b){
-            inicio=b;
-            fin=a;
-        }else{
-            inicio=a;
-            fin=b;
-        }
-        for (int i = inicio+1; i < fin; i++) {
-            suma+=i;
-        }
-            return suma;
-    }
 
 
 }

b0c99e4a76efaad48272139a6b895cb1417774bf
Sandra <docenciasandrag@gmail.com>
2020-07-24 17:40:41 +0200

    practica 12 realización 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,72 @@
 package com.company;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
-import java.util.Stack;
 
 public class Main {
 
 
     public static void main(String[] args) {
 
-        Pila pila = new Pila("FIFO");
-        pila.push("hola");
-        pila.push("mundo");
-        System.out.println(pila.pop());
-        System.out.println(pila.pop());
+        Empleado empleado = new Empleado();
+        Scanner leer = new Scanner(System.in);
+
+        int opcion;
+        String menu="Menú\n "+
+                    "1. Añadir nota \n "+
+                    "2. Mostrar media \n" +
+                    "3. Total aprobados\n" +
+                    "4. Mostrar notas introducidas\n" +
+                    "5. Salir";
+
+        do{
+            System.out.println(menu);
+             opcion = leer.nextInt();
+                leer.nextLine();
+                switch (opcion) {
+                    case 1:
+                        System.out.println("Introduce la nota");
+                        int nota=leer.nextInt();
+                        leer.nextLine();
+                        empleado.añadirNota(nota);
+                        break;
+                    case 2:
+                        mostrarMedia(empleado);
+                        break;
+                    case 3:
+                        totalAprobados();
+                        break;
+                    case 4:
+                        mostrarNotasIntroducidas(empleado);
+                        int[] arrayNotas=empleado.mostrarNotas();
+                        System.out.println("Listado de notas:");
+                        for (int i = 0; i < arrayNotas.length; i++) {
+                        }
+                        System.out.println("");
+                        break;
+                    case 5:
+                        break;
+                    default:
+                        System.out.println("opción no válida");
+
+            }
+        } while (opcion!=5);
 
     }
+
+    private static void mostrarNotasIntroducidas(Empleado empleado) {
+    }
+
+    private static void totalAprobados() {
+    }
+
+    private static void mostrarMedia(Empleado empleado) {
+        System.out.println(empleado.mostrarMedia());
+    }
+
+   
 }
+
 
 

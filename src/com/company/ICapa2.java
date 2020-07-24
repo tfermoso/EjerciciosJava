3c7dbdb2a0c0f97816b5778b2b53f09076093b25
Jose Molinos <molinossobrino.jose@gmail.com>
2020-07-24 17:36:50 +0200

    Practica 12, Algoritmo 2 resuelto

---
+++ src/com/company/ICapa2.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public interface ICapa2 {
+    public void añadirNota(int nota);
+
+    public int mostrarMedia();
+
+    public int totalAprobados();
+
+    public int[] mostrarNotas();
+
+
+}

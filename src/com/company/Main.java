cf89e5015e6833a2967a14a15f1fa548ea3149ac
Tomas <tomas.fermoso@gmail.com>
2020-07-24 20:20:33 +0200

    Práctica 13 algoritmo 2 Corrección error métodos subclase

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,12 @@ package com.company;
 public class Main {
     public  static void main(String[] args){
         CuentaMovimiento cuenta=new CuentaMovimiento(100);
-        cuenta.ingresar(300);
-        cuenta.ingresar(1000);
-        cuenta.ingresar(460);
-        cuenta.retirar(1200);
-        cuenta.retirar(3000);
-        cuenta.ingresar(400);
+        cuenta.ingresarM(300);
+        cuenta.ingresarM(1000);
+        cuenta.ingresarM(460);
+        cuenta.retirarM(1200);
+        cuenta.retirarM(3000);
+        cuenta.ingresarM(400);
         String[][] movimientos=cuenta.consultarMovimientos();
         for (int i = 0; i < movimientos.length; i++) {
             System.out.println("Tipo: "+movimientos[i][0]+", cantidad: "+movimientos[i][1]);

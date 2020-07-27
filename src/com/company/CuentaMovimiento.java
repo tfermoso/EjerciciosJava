b881afe55143f391ec056c021eecf63c64be029f
Tomas <tomas.fermoso@gmail.com>
2020-07-27 20:31:05 +0200

    Práctica 13 algoritmo 2 Evolución CuentaMovimiento y programa principal

--- src/com/company/CuentaMovimiento.java
+++ src/com/company/CuentaMovimiento.java
@@ -3,38 +3,72 @@ package com.company;
 public class CuentaMovimiento extends Cuenta {
     private String[][] movimientos;
     private int contador;
+    private boolean bloqueado;
 
     public CuentaMovimiento(double saldo) {
         super(saldo);
-        this.movimientos=new String[5][2];
+        this.movimientos=new String[5][4];
         this.contador=0;
+        bloqueado=false;
     }
 
     public void ingresarM(double cantidad){
-        this.ingresar(cantidad);
-        registrarMovimiento("Ingresar",cantidad);
+        if(cantidad>0){
+            this.ingresar(cantidad);
+            registrarMovimiento("Ingresar",cantidad,true,"");
+        }else{
+            registrarMovimiento("Ingresar",cantidad,false,"Cantidad negativa");
+        }
+
     }
     public void retirarM(double cantidad){
-        this.retirar(cantidad);
-        registrarMovimiento("Retirar",cantidad);
+        if(saldo>=cantidad & bloqueado==false){
+            this.retirar(cantidad);
+            registrarMovimiento("Retirar",cantidad,true,"");
+        }else if(bloqueado==true){
+            registrarMovimiento("Retirar",cantidad,false,"cuenta bloqueada");
+        }else {
+            registrarMovimiento("Retirar",cantidad,false,"saldo insuficiente");
+
+        }
+
     }
 
-    private void registrarMovimiento(String tipo,double cantidad){
+    private void registrarMovimiento(String tipo,double cantidad,boolean op,String msg){
         if(contador<5){
             movimientos[contador][0]=tipo;
             movimientos[contador][1]=String.valueOf(cantidad);
+            movimientos[contador][2]=String.valueOf(op);
+            movimientos[contador][3]=msg;
             contador++;
         }else{
             for (int i = 1; i < movimientos.length ; i++) {
                 movimientos[i-1][0]=movimientos[i][0];
                 movimientos[i-1][1]=movimientos[i][1];
+                movimientos[i-1][2]=movimientos[i][2];
+                movimientos[i-1][3]=movimientos[i][3];
             }
             contador=4;
-            registrarMovimiento(tipo,cantidad);
+            registrarMovimiento(tipo,cantidad,op,msg);
         }
     }
     public String[][] consultarMovimientos(){
         return movimientos;
+    }
+
+    public boolean resultadoUltimaOperacion(){
+        return Boolean.valueOf(movimientos[contador-1][2]);
+    }
+
+    public void bloquear(){
+        this.bloqueado=true;
+    }
+    public void desbloquear(){
+        this.bloqueado=false;
+    }
+
+    public double consultarSaldo(){
+        return saldo;
     }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,77 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
     public  static void main(String[] args){
-        CuentaMovimiento cuenta=new CuentaMovimiento(100);
-        cuenta.ingresarM(300);
-        cuenta.ingresarM(1000);
-        cuenta.ingresarM(460);
-        cuenta.retirarM(1200);
-        cuenta.retirarM(3000);
-        cuenta.ingresarM(400);
-        String[][] movimientos=cuenta.consultarMovimientos();
-        for (int i = 0; i < movimientos.length; i++) {
-            System.out.println("Tipo: "+movimientos[i][0]+", cantidad: "+movimientos[i][1]);
+        Scanner leer=new Scanner(System.in);
+        CuentaMovimiento cuenta=new CuentaMovimiento(0);
+        String menu="Operaciones disponibles\n" +
+                "1. Ingresar dinero\n" +
+                "2. Retirar dinero\n" +
+                "3. Consultar saldo\n" +
+                "4. Últimos movimientos\n" +
+                "5. Bloquear cuenta\n" +
+                "6. Desbloquar cuenta\n" +
+                "7. Salir\n";
+        int opcion;
+
+        do{
+            System.out.println(menu);
+            opcion=leer.nextInt();
+            leer.nextLine();
+            switch (opcion){
+                case 1:
+                    ingresarDinero(cuenta,leer);
+                    break;
+                case 2:
+                    System.out.println("Cantidad a retirar:");
+                    double cantidad=leer.nextDouble();
+                    leer.nextLine();
+                    cuenta.retirarM(cantidad);
+                    mostrarResultadoUltimaOp(cuenta);
+                    break;
+                case 3:
+                    System.out.println("Su saldo es: ");
+                    System.out.println(cuenta.consultarSaldo() +" €");
+                    break;
+                case 4:
+                    String[][] movimientos= cuenta.consultarMovimientos();
+                    for (int i = 0; i < movimientos.length; i++) {
+                        System.out.println("Tipo: "+movimientos[i][0]+", cantidad: "+movimientos[i][1]+", Resul: "+movimientos[i][2]+", Msg: "+movimientos[i][3]);
+                    }
+                    break;
+                case 5:
+                    cuenta.bloquear();
+                    break;
+                case 6:
+                    cuenta.desbloquear();
+                    break;
+                case 7:
+                    System.out.println("Hasta pronto..");
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+                    break;
+            }
+
+        }while(opcion!=7);
+
+    }
+
+    private static void ingresarDinero(CuentaMovimiento cuenta, Scanner leer) {
+        System.out.println("Cantidad a ingresar: ");
+        double cantidad=leer.nextDouble();
+        leer.nextLine();
+        cuenta.ingresarM(cantidad);
+        mostrarResultadoUltimaOp(cuenta);
+    }
+
+    private static void mostrarResultadoUltimaOp(CuentaMovimiento cuenta){
+        if(cuenta.resultadoUltimaOperacion()){
+            System.out.println("Operación realizada con éxito");
+        }else{
+            System.out.println("Error al realizar la operación");
         }
     }
 }
